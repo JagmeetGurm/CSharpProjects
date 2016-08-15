8ab621b78cec84c8f4df84f1d41d3e1d06df6bdf
JagmeetGurm <jagmeetgurm@yahoo.com>
2016-08-14 19:53:34 -0700

    Add project files.

---
+++ DelegateEventHandlerExample/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+/* This is a delegate and event handler example
+ * One example is of custom delegate and event handler. 
+ * The other is of in-built delegate and event handler.
+ * */
+namespace DelegateEventHandlerExample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
