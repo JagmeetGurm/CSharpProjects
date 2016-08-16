73da57f9f9f26d7ea925496c38bbb159961fda3a
JagmeetGurm <jagmeetgurm@yahoo.com>
2016-08-15 20:28:50 -0700

    Form outputs the delegate result

--- DelegateEventHandlerExample/MainWindow.xaml.cs
+++ DelegateEventHandlerExample/MainWindow.xaml.cs
@@ -19,9 +19,9 @@ using System.Windows.Shapes;
  * */
 namespace DelegateEventHandlerExample
 {
-    /// <summary>
+    
     /// Interaction logic for MainWindow.xaml
-    /// </summary>
+   
     public partial class MainWindow : Window
     {
         public MainWindow()
